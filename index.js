0b99d439db34a5fca453175189972f619ef899da
Sung Bae <singsang2@gmail.com>
2018-09-25 18:02:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, {[key]:value})
+  var newObj = Object.assign({}, object)
+  newObj[key] = value
+  return newObj
 }
 
-function 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+}
+
